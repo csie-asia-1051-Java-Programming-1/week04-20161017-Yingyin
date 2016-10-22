77866d4f9f019cb74c9e77fa4ab298b12678c3fb
Yingyin <105791120@live.asia.edu.tw>
2016-10-22 14:09:04 +0800

    hw01 105791120 杨钰莹

--- src/hw/hw01_1050210XX.java
+++ src/hw/hw01_1050210XX.java
@@ -1,10 +1,23 @@
 package hw;
 
+import java.util.Scanner;
+
 public class hw01_1050210XX {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sca = new Scanner(System.in);
+		int n=sca.nextInt();
+		int arr[]=new int[1000];
+		arr[1]=1;
+		arr[2]=1;
+		for(int i=3;i<=n;i++)
+		{
+			arr[i]=arr[i-1]+arr[i-2];
+		}
+	    for(int i=1;i<=n;i++)
+	    {
+	    	System.out.print(arr[i]+" ");
+	    }
 	}
 
 }
