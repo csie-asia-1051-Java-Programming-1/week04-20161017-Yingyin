9cc7881297bfaa95e4014c953dec320c0f5e6750
Yingyin <105791120@live.asia.edu.tw>
2016-10-23 10:37:31 +0800

    hw02 105791120 杨钰莹

--- src/hw/hw02_1050210XX.java
+++ src/hw/hw02_1050210XX.java
@@ -1,10 +1,27 @@
 package hw;
 
+import java.util.Scanner;
+
 public class hw02_1050210XX {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sca = new Scanner(System.in);
+		int n=sca.nextInt();
+		int m=sca.nextInt();
+		System.out.println(C(n,m));
+	}
+	private static char[] C(int n, int m) {
+		int temp=m-n;
+		int sumN=1,sumM=1;
+		for(int i=n+1;i<m;i++)
+		{
+			sumN*=i;
+		}
+		for(int i=2;i<temp;i++)
+		{
+			sumM*=i;
+		}
+		return null;
 	}
 
 }
